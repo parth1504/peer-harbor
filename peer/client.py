27f5badc2f5bbb6b654fe1ed257e6e717b6ff50d
Sahil Samel <sahilsamel134@gmail.com>
2023-11-25 14:40:13 +0530

    Modification to packages

--- peer/client.py
+++ peer/client.py
@@ -12,13 +12,6 @@ sys.path.append("D:/backend/p2p/peer-harbor")
 
 from utils.manipulation import Piecify
 
-
-
-
-hasher= Piecify("D:/backend/p2p/peer-harbor/README.md",10)
-piece_hashes, piece_indices=hasher._generate_piece_data()
-print(piece_hashes,piece_indices)
-
 # Example usage:
 client_info = ClientInfo()
 client_info.create_file_info("text",7)
@@ -44,24 +37,3 @@ def send_file(conn, file_path):
             conn.send(data)
             data = file.read(chunk_size)
             print("sahil gay")
-
-
-# Example usage
-tracker_url = "http://127.0.0.1:6969/announce"
-info_hash = "another_random_hash"  # Replace with the actual info hash
-peer_id = "abcdefghij0123456789"  # Replace with the actual peer ID
-ip = "127.0.0.1"  # Replace with the actual IP address of your client
-port = 6881  # Replace with the actual port number
-uploaded = 0  # Replace with the actual amount uploaded by your client
-downloaded = 0  # Replace with the actual amount downloaded by your client
-left = 1024  # Replace with the actual amount left to download by your client
-
-# peer_list = get_peer_list(tracker_url, info_hash, peer_id, ip, port, uploaded, downloaded, left)
-# if peer_list:
-#     print(f"Peer list from tracker: {peer_list.decode('utf-8')}")
-
-connect_to_peer("127.0.0.1",6881)
-
-
-
-
