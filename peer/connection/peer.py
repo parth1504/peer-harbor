b7df91145fc459cb83656f7a8923459bed344c2b
Sahil Samel <sahilsamel134@gmail.com>
2023-11-27 15:56:12 +0530

    Refactored code

--- peer/connection/peer.py
+++ peer/connection/peer.py
@@ -4,23 +4,23 @@ import sys,os
 current_file_path = os.path.abspath(__file__)
 project_root = os.path.dirname(os.path.dirname(current_file_path))
 sys.path.append(project_root)
-sys.path.append("D:/backend/p2p/peer-harbor")
+     
+class PeerConnection:
+    def __init__(self, peer_ip, peer_port):
+        self.peer_ip = peer_ip 
+        self.peer_port = peer_port 
+ 
+    def seed_connection(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        server_socket.bind((self.peer_ip, self.peer_port)) 
+        server_socket.listen(1) 
+        client_socket, client_address = server_socket.accept()
+        return client_socket, client_address, server_socket
 
-from  strategies.pieceSelectionAlgorithm import receive_bitset
-from  strategies.pieceSelectionAlgorithm import send_bitset
-
-
-def connect_to_peer(peer_ip, peer_port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    message="bitch ass nigga"
-
-    try:
-        client_socket.connect((peer_ip, peer_port))
-        print(f"Connected to {peer_ip}:{peer_port}")
+    def leecher_connection(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.peer_ip, self.peer_port))
         return client_socket
-
-    except Exception as e:
-        print(f"Error: Unable to connect to {peer_ip}:{peer_port}. {e}")
-
-    #finally:
-       # client_socket.close()
+    
+def close_connection(self, socket):
+    socket.close()
--- peer/testPeer.py
+++ peer/testPeer.py
@@ -6,132 +6,22 @@ current_file_path = os.path.abspath(__file__)
 project_root = os.path.dirname(os.path.dirname(current_file_path))
 sys.path.append(project_root)
 
-from connection.peer import connect_to_peer
 from utils.manipulation import Piecify
-
-
-def send_pieces(socket, index, pieces):
-    if not socket:
-        raise ValueError("Socket not connected")
-
-    for piece_index, piece_data in zip(index, pieces):
-        # Serialize the piece data
-        serialized_index = struct.pack('!Q', piece_index)
-        serialized_piece = struct.pack(f'!{len(piece_data)}s', piece_data)
-
-        # Calculate the hash of the serialized data
-        hash_piece = hashlib.sha1(serialized_index + serialized_piece).digest()
-        print(len(serialized_index) , len(serialized_piece),len(hash_piece))
-        # Send the index, piece_data, and hash_piece
-        serialized_data = serialized_index + serialized_piece + hash_piece
-        print(len(serialized_data))
-        socket.sendall(serialized_data)
-
-    # Indicate the end of pieces transmission
-    socket.sendall(b'TERMINATE')
-
-def receive_pieces(socket):
-    if not socket:
-        raise ValueError("Socket not connected")
-
-    received_data = b''
-
-    # Adjust the buffer size as needed
-    buffer_size = 4096
-
-    while True:
-        data = socket.recv(buffer_size)
-
-        if not data:
-            break  # Connection closed
-
-        received_data += data
-
-        # Check if the end of pieces transmission is reached
-        if received_data.endswith(b'TERMINATE'):
-            received_data = received_data[:-len(b'TERMINATE')]
-            break
-
-    # Unpack the received data
-    index = []
-    pieces = []
-
-    while len(received_data) >= 28:  # Minimum length for a valid message (8 + len(piece_data) + 20)
-        # Unpack the header
-        header_format = '!Q'
-        header_size = struct.calcsize(header_format)
-        index_value = struct.unpack(header_format, received_data[:header_size])[0]
-
-        # Unpack the piece data
-        piece_data_size = len(received_data) - 28
-        piece_data_format = f'!{piece_data_size}s20s'
-        piece_data = struct.unpack(piece_data_format, received_data[header_size:])
-
-        # Validate the received data by checking the hash
-        hash_piece = hashlib.sha1(struct.pack(f'!Q{piece_data_size}s', index_value, piece_data[0])).digest()
-        if hash_piece != piece_data[1]:
-            raise ValueError("Hash mismatch. Data may be corrupted.")
-
-        # Append the index and piece data to the lists
-        index.append(index_value)
-        pieces.append(piece_data[0])
-
-        # Remove processed data from the received_data buffer
-        received_data = received_data[header_size + piece_data_size + 20:]
-
-    return index, pieces
-
-def simulate_peer(peer_ip, peer_port):
-    # Simulate peer behavior
-    print(f"Simulating peer at {peer_ip}:{peer_port}")
-    
-    # Simulate some activity or data exchange
-    time.sleep(2)
-
-    # Connect to the other peer
-    connect_to_peer("127.0.0.1", 6881)  # Replace with the actual IP and port of the other peer
-
-
-def receive_file(conn, file_path):
-    with open(file_path, 'wb') as file:
-        chunk_size = 10
-        data = conn.recv(chunk_size)
-        while data:
-            file.write(data)
-            data = conn.recv(chunk_size)
-
-def send_bitset(peer_socket, bitset):
-    message = {'type': 'bitset', 'data': bitset}
-    peer_socket.sendall(json.dumps(message).encode())
-
-
-def receive_bitset(peer_socket):
-    data = peer_socket.recv(1024)
-    if data:
-        message = json.loads(data.decode())
-        if message['type'] == 'bitset':
-            return message['data']
-    return None
+from connection.peer import PeerConnection, close_connection
+from upload.Seed import send_pieces
 
 def start_server(ip, port):
-    hasher= Piecify("D:/backend/p2p/peer-harbor/README.md",20)
-    pieces,piece_hashes, piece_indices=hasher._generate_piece_data()
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((ip, port))
-    server_socket.listen(1)  # Listen for one incoming connection
-
-    print(f"Waiting for incoming connection on {ip}:{port}")
-    client_socket, addr = server_socket.accept()
-    print(f"Accepted connection from {addr}")
-    # Receive and print messages
-    #receive_file(client_socket,"D:/backend/p2p/peer-harbor/temp.md")
-   
-    send_pieces(client_socket, piece_indices,pieces)
-    client_socket.close()
-    server_socket.close()
+    hasher= Piecify("./output.torrent",20)
+    pieces, piece_hashes, piece_indices=hasher._generate_piece_data()
+    
+    client = PeerConnection(ip, port)
+    
+    client_socket, client_address, server_socket = client.seed_connection()
+    send_pieces(client_socket, piece_indices, pieces)
+    close_connection(client_socket)
+    close_connection(server_socket)
 
 if __name__ == "__main__":
-    # Adjust the IP and port to match the server's configuration
     server_ip = "127.0.0.1"
     server_port = 6881
     start_server(server_ip, server_port)
