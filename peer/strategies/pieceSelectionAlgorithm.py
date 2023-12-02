cd21f4f94d35e64cf176c2124f91bada1bac3458
“Daksha <dakshaaeer@gmail.com>
2023-12-02 11:56:09 +0530

    added piece selection

--- peer/strategies/pieceSelectionAlgorithm.py
+++ peer/strategies/pieceSelectionAlgorithm.py
@@ -1,5 +1,5 @@
 import json
-
+import time
 
 class ClientInfo:
     def __init__(self):
@@ -32,7 +32,7 @@ class ClientInfo:
     def get_file_needed_pieces(self, file_id):
         # Return the set of pieces the client still needs for a specific file
         return self.file_info[file_id]['needed_pieces']
-    
+
 def send_bitset(peer_socket, bitset):
     message = {'type': 'bitset', 'data': bitset}
     peer_socket.sendall(json.dumps(message).encode())
@@ -44,3 +44,32 @@ def receive_bitset(peer_socket):
         if message['type'] == 'bitset':
             return message['data']
     return None
+
+
+class RarityTracker:
+    def init(self, num_pieces, base_smoothing_factor=0.2):
+        self.num_pieces = num_pieces
+        self.base_smoothing_factor = base_smoothing_factor
+        self.piece_metrics = {index: {'rarity': 5, 'base_smoothing_factor': base_smoothing_factor} for index in range(num_pieces)}
+
+    def update_rarity(self, piece_index, accept=True, network_size=1):
+        if piece_index in self.piece_metrics:
+            metric = self.piece_metrics[piece_index]
+            target_value = 1 if accept else 10
+            smoothing_factor = metric['base_smoothing_factor'] / network_size
+            metric['rarity'] = (1 - smoothing_factor) * metric['rarity'] + smoothing_factor * target_value
+
+    def get_rarity(self, piece_index):
+        if piece_index in self.piece_metrics:
+            metric = self.piece_metrics[piece_index]
+            return min(10, max(1, metric['rarity']))
+        else:
+            return None  # Handle the case where the piece index is not found
+
+    def refresh(self, initial_rarity_values):
+        for index in range(self.num_pieces):
+            if index < len(initial_rarity_values):
+                self.piece_metrics[index]['rarity'] = initial_rarity_values[index]
+            else:
+                # Handle the case where the external array is shorter than the number of pieces
+                self.piece_metrics[index]['rarity'] = 5  # Reset to a default value
